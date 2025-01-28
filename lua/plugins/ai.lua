a9c5701d214ad2a1b011eb51e69243e9f7cb9c39
Sandeeb Adhikari <sa2734@njit.edu>
2025-01-28 12:32:20 -0500

    add key map to ai intergration

--- lua/plugins/ai.lua
+++ lua/plugins/ai.lua
@@ -3,7 +3,13 @@ return {
   lazy = false,
   config = function()
     require("copilot").setup({
-      suggestion = { enabled = true },
+      suggestion = { 
+        enabled = true,
+        keymap = {
+          accept = "<C-j>", -- Accept Copilot suggestion
+          dismiss = "<C-k>", -- Dismiss Copilot suggestion
+        },
+      },
       panel = { enabled = true },
     })
   end,
